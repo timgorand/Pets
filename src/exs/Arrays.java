336c0fc88c75eecf92949b86b5e05db9b3ebead5
timgorand <timgorand@mail.ru>
2022-06-22 17:19:51 +0300

    first commit

--- src/exs/Arrays.java
+++ src/exs/Arrays.java
@@ -6,34 +6,38 @@ import java.util.Random;
 Пример:
 Дан массив [1, 2, 3], и массив весов [1, 2, 10].
 В среднем, значение «1» должно возвращаться в 2 раза реже, чем значение «2» и в десять раз реже, чем значение «3».*/
+// т.е. получится массив [1223333333333] в котором нужно будет выбрать 1 цифру по индексу
 public class Arrays {
-    private int [] numbers;
-    private int [] weights;
-    private int [] final_array;
-    private  int left_index=0;
-    private int sum=0;
 
-    public void takes (int [] numbers_p, int[]weights_p)
+    public static int takes(int[] numbers_p, int[] weights_p)
     {
-        numbers = numbers_p;
-        weights = weights_p;
-        for (int weight : weights)
+        int left_index=0;
+        int sum=0;
+        for (int weight : weights_p)
         {
             sum += weight;
         }
-        final_array = new int[sum];
-        for(int i = 0; i<weights.length; i++)
+        int [] final_array = new int[sum];
+        for(int i = 0; i<weights_p.length; i++)
         {
-            for(int j =0; j<weights[i]; j++)
+            for(int j =0; j<weights_p[i]; j++)
             {
-                final_array[j+left_index]=numbers[i];
+                final_array[j+left_index]=numbers_p[i];
             }
-            left_index += weights[i];
+            left_index += weights_p[i];
         }
+        return get_Random(final_array);
     }
-    public int get_Random ()
+    public static int get_Random(int[] array) //Без статиков ни takes, ни get_Random НЕ хотят работать с main
     {
         Random r = new Random();
-        return (final_array[r.nextInt(0,final_array.length-1)]);
+        return (array[r.nextInt(0,array.length-1)]);
+    }
+
+    public static void main(String[] args)
+    {
+        int [] numbers = new int [] {1,5,2,9};
+        int [] weight = new int [] {3,2,7,4};
+        System.out.println(takes(numbers, weight));
     }
 }
